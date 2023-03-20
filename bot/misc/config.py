b5ebc36e5dbc4165a1359f5665a7d1edcb48f914
thirtysix <assbreak3r@gmail.com>
2023-03-20 20:34:52 +0300

    Update remove deprecated parameteres.

--- bot/misc/config.py
+++ bot/misc/config.py
@@ -6,10 +6,6 @@ class Config:
     TOKEN: Final = environ.get('TOKEN', 'define me')
     DEBUG: Final = bool(len(environ.get('DEBUG', '')))
 
-    DB_NAME: Final = 'jokes.db'
-    DB_TABLE_JOKES: Final = 'jokes'
-    DB_TABLE_USERS: Final = 'users'
-
     # Label, that means 'random category'
     RAND_CATEGORY: Final = 'СЛУЧАЙНАЯ'
     RAND_BTN: Final = 'Случайный анекдот'

38ef71cfbda1fe327fa8cbd98b80a7520fa1a794
thirtysix <assbreak3r@gmail.com>
2023-03-24 15:59:01 +0300

    Add path control for resolving logs path.

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,11 +2,10 @@ from loguru import logger
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-import bot.database
-# from bot.filters import register_all_filters
 from bot.misc import Config
 from bot.handlers import register_all_handlers
 from bot.database.models import register_models
+from bot.misc.pathcontrol import PathControl
 
 
 async def __on_start_up(dp: Dispatcher) -> None:
@@ -23,7 +22,7 @@ def __start_telegram_bot() -> None:
 
 def __main() -> None:
     if not Config.DEBUG:
-        log_path = 'bot.log'
+        log_path = PathControl.get('bot/logs/{time}.log')
         logger.add(log_path, format='{time} {level} {message}', rotation='10:00', compression='zip', retention='3 days')
     __start_telegram_bot()
 
---
+++ bot/misc/pathcontrol.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from typing import Final
+from pathlib import Path
+
+
+class PathControl(ABC):
+    ROOT: Final = Path(__file__).parent.parent.parent
+
+    @classmethod
+    def get(cls, path: str) -> Path:
+        return cls.ROOT.joinpath(path)

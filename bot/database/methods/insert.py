6dd9c73de46cc94a942f69ab00cb63bf3a465936
thirtysix <assbreak3r@gmail.com>
2023-04-08 21:45:31 +0300

    Fix unique joke filter crash.

--- bot/database/methods/insert.py
+++ bot/database/methods/insert.py
@@ -1,8 +1,14 @@
+from sqlalchemy.exc import NoResultFound
+
 from bot.database import Database
 from bot.database.models import Joke
 
 
 async def save_joke(jokes: list[str, str], category: str) -> None:
+    session = Database().session
     for joke in jokes:
-        Database().session.add(Joke(joke=joke, category=category))
-    Database().session.commit()
+        try:
+            session.query(Joke.joke).filter(Joke.joke == joke).one()
+        except NoResultFound:
+            session.add(Joke(joke=joke, category=category))
+    session.commit()

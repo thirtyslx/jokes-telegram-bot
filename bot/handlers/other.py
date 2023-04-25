b500100630f1369fc0bc1c2d7337042e588db11e
thirtysix <assbreak3r@gmail.com>
2023-04-26 00:39:02 +0300

    Fix start command payload verification.

--- bot/handlers/other.py
+++ bot/handlers/other.py
@@ -3,20 +3,22 @@ from aiogram.types import Message
 from aiogram.utils.deep_linking import decode_payload
 
 from bot.database.methods.create import create_user
+from bot.database.methods.get import get_categories
 from bot.keyboards import ReplyKb
 from bot.misc import Config
 from bot.handlers.user import change_category
 
 
-async def __cmd_start(message: Message):
+async def __cmd_start(message: Message) -> None:
     create_user(message.from_user.id)
     args = decode_payload(message.get_args())
-    if not args:
-        await message.answer('Анекдоты',
-                             reply_markup=ReplyKb.get_main(Config.RAND_CATEGORY))
-    elif args.startswith('change-category-'):
+    if args and args.startswith('change-category-'):
         category = args.lstrip('change-category-')
-        await change_category(message, category)
+        if category in get_categories():
+            await change_category(message, category)
+            return
+    await message.answer('Анекдоты',
+                         reply_markup=ReplyKb.get_main(Config.RAND_CATEGORY))
 
 
 async def __help(message: Message):

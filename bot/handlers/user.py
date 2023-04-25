16404995c365ba935a92e7c55fdbd8f4422c109f
thirtysix <assbreak3r@gmail.com>
2023-04-26 01:58:22 +0300

    Add get_joke, get_random_joke commands.

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -3,7 +3,7 @@ from aiogram.types import Message, CallbackQuery, BotCommand
 from aiogram.dispatcher.filters import Text
 from aiogram.utils.deep_linking import get_start_link
 
-from bot.database.methods.get import get_joke_by_category, get_categories
+from bot.database.methods.get import get_joke_by_category, get_categories, get_default_category
 from bot.database.methods.update import set_default_category
 from bot.keyboards import ReplyKb, InlineKb
 from bot.misc import Config
@@ -28,8 +28,10 @@ async def __send_joke(message: Message, joke: str,
 
 
 async def __handle_random_joke(message: Message):
-    if message.text == Config.RAND_BTN:
+    if message.text == '/get_random_joke' or message.text == Config.RAND_BTN:
         category = Config.RAND_BTN
+    elif message.text == '/get_joke':
+        category = get_default_category(message.from_user.id)
     else:
         category = message.text.lstrip('Случайный ')[1:-1]
     joke, joke_category = get_joke_by_category(category)
@@ -56,14 +58,14 @@ async def __cmd_change_category(message: Message):
         await message.answer('\n'.join((b(f'Ошибка: {code(category)}: Не найдено такой категории'),
                                         '',
                                         b('Чтобы получить список доступных категорий, вы можете использовать:'),
-                                        code('/list_categories'))),
+                                        '/list_categories')),
                              reply_markup=ReplyKb.get_main(Config.RAND_CATEGORY))
     else:
         await message.answer('\n'.join((u(b('Использование:')),
                                         code("/change_category [category]"),
                                         '',
                                         b('Чтобы получить список доступных категорий, вы можете использовать:'),
-                                        code('/list_categories'))),
+                                        '/list_categories')),
                              reply_markup=ReplyKb.get_main(Config.RAND_CATEGORY))
 
 
@@ -75,6 +77,8 @@ async def __cmd_list_categories(message: Message):
 
 def get_user_commands(dp: Dispatcher):
     return (
+        ('/get_joke', 'Шутка из текущей категории.'),
+        ('/get_random_joke', 'Шутка из случайной категории.'),
         ('/change_category', 'Изменить категорию.'),
         ('/list_categories', 'Список всех доступных категорий.'),
     )
@@ -85,6 +89,7 @@ def register_user_handlers(dp: Dispatcher) -> None:
     dp.register_message_handler(__handle_list_categories, Text(equals='Изменить Категорию'))
     dp.register_message_handler(__cmd_change_category, commands=['change_category', 'cc'])
     dp.register_message_handler(__cmd_list_categories, commands=['list_categories', 'lc'])
+    dp.register_message_handler(__handle_random_joke, commands=['get_joke', 'get_random_joke'])
 
     dp.register_callback_query_handler(__handle_change_category,
                                        lambda c: c.data and c.data.startswith('change-category-'))
